0fb976005399792fceb131d8fca8adabb1a5bb1c
Sandesh Pokhrel <sandesh.pokhrel56@gmail.com>
2020-11-13 22:17:44 +0545

    Reverted port to 8080 and removed some unnecessary console log statements

--- src/main/java/bqtest/web/FileController.java
+++ src/main/java/bqtest/web/FileController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin(origins = *, maxAge = 3600)
 public class FileController {
 
     private static final Logger log = LoggerFactory.getLogger(FileController.class);
--- src/main/webapp/script/testApp.js
+++ src/main/webapp/script/testApp.js
@@ -1,14 +1,11 @@
 angular.module('bqTestModule', [])
     .controller('FetchController', ['$scope', '$http',
         function ($scope, $http) {
-	    	$scope.memberData = [];
 	    	fetchData();
             function fetchData() {
-                $http({method: 'GET', url: 'http://localhost:8989/test/api/load-data'}).then(function (response) {
-                	console.log(response.data);
+                $http({method: 'GET', url: 'http://localhost:8080/test/api/load-data'}).then(function (response) {
                 	$scope.memberData = response.data;
                     $scope.memberArray = Array.prototype.concat.apply([], Object.keys($scope.memberData).map((key) => $scope.memberData[key]));
-                    console.log($scope.memberArray);
                 }, function (reason) {
                     console.log('error ' + reason)
                 });
